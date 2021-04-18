cdc59b0af558e81806e6af715e634d95aa810a12
Coal33 <takasum88@outlook.jp>
2021-04-18 22:24:16 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class PinyinAssist
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxinput = new System.Windows.Forms.TextBox();
+            this.textBoxoutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxinput
+            // 
+            this.textBoxinput.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxinput.Location = new System.Drawing.Point(21, 58);
+            this.textBoxinput.Multiline = true;
+            this.textBoxinput.Name = "textBoxinput";
+            this.textBoxinput.Size = new System.Drawing.Size(759, 100);
+            this.textBoxinput.TabIndex = 0;
+            this.textBoxinput.TextChanged += new System.EventHandler(this.textBoxinput_TextChanged);
+            // 
+            // textBoxoutput
+            // 
+            this.textBoxoutput.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxoutput.Location = new System.Drawing.Point(21, 284);
+            this.textBoxoutput.Multiline = true;
+            this.textBoxoutput.Name = "textBoxoutput";
+            this.textBoxoutput.ReadOnly = true;
+            this.textBoxoutput.Size = new System.Drawing.Size(759, 100);
+            this.textBoxoutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(17, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "入力";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(17, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "出力";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(38, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(684, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "※a1→āのように変換されます。üはv0で入力可能です。(声調記号付きのüは適宜v1~4に変更してください)";
+            // 
+            // PinyinAssist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxoutput);
+            this.Controls.Add(this.textBoxinput);
+            this.Name = "PinyinAssist";
+            this.Text = "PinyinAssist";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxinput;
+        private System.Windows.Forms.TextBox textBoxoutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class PinyinAssist : Form
+    {
+        public PinyinAssist()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBoxinput_TextChanged(object sender, EventArgs e)
+        {
+            string s = textBoxinput.Text;
+
+            //aに関して置き換え
+            s = s.Replace("a1", "ā");
+            s = s.Replace("a2", "á");
+            s = s.Replace("a3", "ǎ");
+            s = s.Replace("a4", "à");
+
+            //iに関して置き換え
+            s = s.Replace("i1", "ī");
+            s = s.Replace("i2", "í");
+            s = s.Replace("i3", "ǐ");
+            s = s.Replace("i4", "ì");
+
+            //uに関して置き換え
+            s = s.Replace("u1", "ū");
+            s = s.Replace("u2", "ú");
+            s = s.Replace("u3", "ǔ");
+            s = s.Replace("u4", "ù");
+
+
+            //eに関して置き換え
+            s = s.Replace("e1", "ē");
+            s = s.Replace("e2", "é");
+            s = s.Replace("e3", "ě");
+            s = s.Replace("e4", "è");
+
+            //oに関して置き換え
+            s = s.Replace("o1", "ō");
+            s = s.Replace("o2", "ó");
+            s = s.Replace("o3", "ǒ");
+            s = s.Replace("o4", "ò");
+
+            //üに関して置き換え
+            s = s.Replace("v0", "ü");
+            s = s.Replace("v1", "ǖ");
+            s = s.Replace("v2", "ǘ");
+            s = s.Replace("v3", "ǚ");
+            s = s.Replace("v4", "ǜ");
+
+            textBoxoutput.Text = s;
+        }
+    }
+}
